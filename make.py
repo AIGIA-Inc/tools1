6599f17f76980ae3fe66a5a327e5ab32e051c4a5
MIKIO ODA <oda.mikio@gmail.com>
2021-11-22 14:09:57 +0900

    ...

--- make.py
+++ make.py
@@ -1,35 +1,43 @@
 import os
 import pathlib
 import shutil
+import pprint
 
-PATH_ROOT = str(pathlib.Path(__file__).resolve().parent)
-PATH_PRODUCT = PATH_ROOT + "/product"
+PATH_ROOT: str = str(pathlib.Path(__file__).resolve().parent)
+PATH_PRODUCT: str = PATH_ROOT + "/product"
 
 
-def build_directory(directoryname):
-    target = PATH_ROOT + directoryname
+def build_directory(directoryname: str) -> str:
+    target: str = PATH_ROOT + directoryname
     if not (os.path.exists(target)):
         os.mkdir(target)
-    return directoryname
+    return target
 
 
-def build_file(filename):
-    shutil.copy2(PATH_ROOT + filename, PATH_PRODUCT + filename)
-    return filename
+def build_file(filename: str)-> str:
+    source: str = PATH_ROOT + filename
+    target: str = PATH_PRODUCT + filename
+    os.remove(target)
+    shutil.copy2(source, target)
+    return target
 
 
 if __name__ == "__main__":
     build_directory("/product")
-    print(list(map(build_directory, ["/product/templates", "/product/static"])))
-    print(list(map(build_file, ["/main.py",
-                                "/requirement.txt",
+    pprint.pprint(list(map(build_directory, ["/product/templates",
+                                     "/product/static",
+                                     "/product/aig_accounts",
+                                     "/product/aig_shots"
+                                     ])))
+    pprint.pprint(list(map(build_file, ["/main.py","/setup.py",
+                                "/requirements.txt",
                                 "/aig_accounts/__init__.py",
                                 "/aig_accounts/accounts.py",
                                 "/aig_accounts/download_accounts.py",
                                 "/aig_accounts/payment.py",
                                 "/aig_shots/__init__.py",
-                                "/aig_accounts/shots.py",
-                                "/aig_accounts/download_shots.py",
+                                "/aig_shots/shots.py",
+                                "/aig_shots/download_shots.py",
                                 "/templates/accounts.html",
                                 "/templates/accounts_tree.html",
                                 "/templates/base.html",
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='tools1',
+    version='0.1.0',
+    packages=['aig_shots', 'aig_accounts'],
+    url='',
+    license='BSD',
+    author='oda',
+    author_email='oda.mikio@gmail.com',
+    description='for aig'
+)
