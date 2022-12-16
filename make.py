c6aa078f98d315b5a5321f9d073030c5e4a8ce15
asano0852 <asano_aigia@icloud.com>
2022-12-16 13:52:24 +0900

    ...

--- make.py
+++ make.py
@@ -3,9 +3,10 @@ import pathlib
 import shutil
 import pprint
 
-#絶対パスで取得
+# 絶対パスで取得
 PATH_ROOT: str = str(pathlib.Path(__file__).resolve().parent)
 PATH_PRODUCT: str = PATH_ROOT + "/product"
+
 
 def build_directory(directoryname: str) -> str:
     target: str = PATH_ROOT + directoryname
@@ -14,16 +15,16 @@ def build_directory(directoryname: str) -> str:
     return target
 
 
-def build_file(filename: str)-> str:
+def build_file(filename: str) -> str:
     result = ""
-    source: str = PATH_ROOT + filename #絶対パスで取得
+    source: str = PATH_ROOT + filename  # 絶対パスで取得
     target: str = PATH_PRODUCT + filename
 
     try:
-    #    os.remove(target) #プロダクト内のファイルを削除
-        shutil.copy2(source, target) #ディレクトリ下へコピー
+        #    os.remove(target) #プロダクト内のファイルを削除
+        shutil.copy2(source, target)  # ディレクトリ下へコピー
     except:
-        result= target +" error."
+        result = target + " error."
 
     return result
 
@@ -31,11 +32,11 @@ def build_file(filename: str)-> str:
 if __name__ == "__main__":
     build_directory("/product")
     pprint.pprint(list(map(build_directory, ["/product/templates",
-                                     "/product/static",
-                                     "/product/aig_accounts",
-                                     "/product/config",
-                                     "/product/data"
-                                     ])))
+                                             "/product/static",
+                                             "/product/aig_accounts",
+                                             "/product/config",
+                                             "/product/data"
+                                             ])))
     pprint.pprint(list(map(build_file, ["/main.py",
                                         "/auth.py",
                                         "/models.py",
@@ -45,9 +46,9 @@ if __name__ == "__main__":
                                         "/config/default.json",
                                         "/static/.gitkeep",
                                         "/data/upload.csv",
-                                "/aig_accounts/__init__.py",
-                                "/aig_accounts/accounts.py",
-                                "/templates/base.j2",
-                                "/templates/error.j2",
-                                "/templates/index.j2",
-                                ])))
+                                        "/aig_accounts/__init__.py",
+                                        "/aig_accounts/accounts.py",
+                                        "/templates/base.j2",
+                                        "/templates/error.j2",
+                                        "/templates/index.j2",
+                                        ])))
